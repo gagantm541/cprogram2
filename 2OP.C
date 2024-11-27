b04d99c124547fdefb8bc0f7a80b6b5e0fcde470
gagantm541 <gagantm2206@gmail.com>
2024-11-27 13:58:03 +0530

    Add files via upload

---
+++ 2OP.C
@@ -0,0 +1,6 @@
+#include<stdio.h>
+void main()
+{
+	int a=7,b=3,c=8,addition,multiplication,subraction,division;
+	int modulus, equivalent,not_equivalent,greater,lesser;
+	int greater_than_and_equal,lesser_than_and_equal;
